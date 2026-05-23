76c87008b8a05e66377a07accbe608c06d3d2aa3
abiwonggrage-a11y <abi.wonggrage@gmail.com>
2026-05-23 10:40:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,17 @@ if st.button("Say hello"):
     st.write("hi pacar dika!")
 else:
     st.write("Goodbye")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
