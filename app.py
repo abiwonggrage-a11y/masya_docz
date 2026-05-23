2110e86f16e472e93fe3e805b7fae5068ce61d9d
abiwonggrage-a11y <abi.wonggrage@gmail.com>
2026-05-23 10:27:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
 
-
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
 
